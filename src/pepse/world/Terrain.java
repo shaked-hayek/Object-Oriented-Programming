ba6f8b18c533d80730290b5d0dc7cc463e288f62
unknown <shaked.hayek@mail.huji.ac.il>
2023-01-04 14:55:23 +0200

    Ground finished

--- src/pepse/world/Terrain.java
+++ src/pepse/world/Terrain.java
@@ -8,7 +8,6 @@ import pepse.util.ColorSupplier;
 import pepse.util.NoiseGenerator;
 
 import java.awt.Color;
-import java.util.Random;
 
 
 public class Terrain {
@@ -18,12 +17,13 @@ public class Terrain {
     private static final int TERRAIN_DEPTH = 20;
     private final float groundHeightAtX0;
     private final int seed;
-    private final Vector2 windowDimensions;
     private GameObjectCollection gameObjects;
     private final int groundLayer;
     private static final String tagName = "ground";
     private static final Color BASE_GROUND_COLOR = new Color(212, 123, 74);
     private static final float GROUND_START_HEIGHT = (float) 2 / 3;
+    private static final int GROUND_SPREAD = 15;
+    private static final int GROUND_SHARPNESS = 6 * Block.SIZE;
 
     public Terrain(GameObjectCollection gameObjects,
                    int groundLayer,
@@ -32,46 +32,34 @@ public class Terrain {
 
         this.gameObjects = gameObjects;
         this.groundLayer = groundLayer;
-        this.windowDimensions = windowDimensions;
-        this.groundHeightAtX0 = windowDimensions.x() * GROUND_START_HEIGHT;
+        this.groundHeightAtX0 = windowDimensions.y() * GROUND_START_HEIGHT;
         this.seed = seed;
 
         createInRange(0, 1280);
     }
 
-    public void createInRange(int minX, int maxX) { // TODO
-        double curr_x = groundHeightAt(groundHeightAtX0);
-        Renderable blockRender = new RectangleRenderable(ColorSupplier.approximateColor(BASE_GROUND_COLOR));
-        //added to create random blocks
-        int minXFixed = (int)(Block.SIZE*(Math.floor((double)minX/Block.SIZE)));
-        int maxXFixed = (int)(Block.SIZE*(Math.ceil((double)maxX/Block.SIZE)));
-        Random rand = new Random();
-        int currMin = minXFixed;
-        int blockRand = 0;
-        for (int i = minXFixed; i < maxXFixed; i+=blockRand) {
-            blockRand = (rand.nextInt((maxXFixed - currMin)/Block.SIZE))*Block.SIZE;
-            Block block = new Block(new Vector2(currMin, windowDimensions.y()-30), blockRender);
-            block.setTopLeftCorner(new Vector2(currMin, windowDimensions.y()-30));
-            gameObjects.addGameObject(block, groundLayer);
-            block.setTag(tagName);
-            currMin+=blockRand;
-            if (maxXFixed-currMin <= Block.SIZE){ //added because otherwise get stuck
-                break;
+    public void createInRange(int minX, int maxX) {
+        int minXFixed = (int) (Block.SIZE * (Math.floor((double) minX / Block.SIZE)));
+        int maxXFixed = (int) (Block.SIZE * (Math.ceil((double) maxX / Block.SIZE)));
+
+        for (int i = minXFixed; i < maxXFixed; i++) {
+            float currentX = minXFixed + (i * Block.SIZE);
+            float topY = groundHeightAt(currentX);
+            for (int j = 0; j < TERRAIN_DEPTH; j++) {
+                Renderable blockRender = new RectangleRenderable(ColorSupplier.approximateColor(BASE_GROUND_COLOR));
+                Block block = new Block(
+                        new Vector2(currentX, topY + j * Block.SIZE),
+                        blockRender
+                );
+                gameObjects.addGameObject(block, groundLayer);
+                block.setTag(tagName);
             }
         }
-        Block block = new Block(new Vector2(currMin, windowDimensions.y()-30), blockRender);
-        block.setTopLeftCorner(new Vector2(currMin, windowDimensions.y()-30));
-        gameObjects.addGameObject(block, groundLayer);
-        block.setTag(tagName); //added in case not all full, not a problem to fill extra
-
-//        Block block = new Block(Vector2.ZERO, blockRender);
-//        Block block = new Block(new Vector2(0, windowDimensions.y()-30), blockRender);
-//        gameObjects.addGameObject(block, groundLayer);
-//        block.setTag("ground");
     }
 
-    public double groundHeightAt(float x) {
+    public float groundHeightAt(float x) {
         NoiseGenerator noiseGenerator = new NoiseGenerator(seed);
-        return noiseGenerator.noise(x);
+        float y = (float) noiseGenerator.noise(x / GROUND_SPREAD);
+        return (y * GROUND_SHARPNESS) + groundHeightAtX0;
     }
 }
