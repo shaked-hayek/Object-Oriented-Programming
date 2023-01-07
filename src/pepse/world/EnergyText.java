a97c71fa4ab985f654a7a17bd126f92493828146
shirlevy <shir.levy2@mail.huji.ac.il>
2023-01-07 21:48:08 +0200

    EnergyText - PERFECT

--- src/pepse/world/EnergyText.java
+++ src/pepse/world/EnergyText.java
@@ -12,7 +12,11 @@ import java.awt.*;
 import static pepse.world.Avatar.getEnergy;
 
 public class EnergyText extends GameObject {
-    public static final Color GREEN = new Color(47, 190, 24);
+    public static final Color GREEN = new Color(47, 200, 30);
+    private static final String TEXT_ENERGY = "Energy Left: %.1f";
+    private static final String FONT_NAME = "Impact";
+    private static final double HIGH_ENERGY = 70;
+    private static final double LOW_ENERGY = 10;
 
 
     private final TextRenderable textRenderable;
@@ -20,7 +24,8 @@ public class EnergyText extends GameObject {
     public EnergyText(Vector2 topLeftCorner, Vector2 dimensions) {
         super(topLeftCorner, dimensions, null);
 
-        TextRenderable textRenderable = new TextRenderable(String.format("Energy Left: %.1f", getEnergy()));
+        TextRenderable textRenderable = new TextRenderable(String.format(TEXT_ENERGY, getEnergy()),
+                FONT_NAME);
         this.renderer().setRenderable(textRenderable);
         textRenderable.setColor(Color.GREEN);
         this.textRenderable = textRenderable;
@@ -31,15 +36,15 @@ public class EnergyText extends GameObject {
     @Override
     public void update(float deltaTime) {
         super.update(deltaTime);
-        if (getEnergy()>80){
+        if (getEnergy()>HIGH_ENERGY){
             textRenderable.setColor(GREEN);
         }
-        else if (getEnergy() < 10) {
+        else if (getEnergy() < LOW_ENERGY) {
             textRenderable.setColor(Color.RED);
         }
         else {
             textRenderable.setColor(Color.YELLOW);
         }
-        textRenderable.setString(String.format("Energy Left: %.1f", getEnergy()));
+        textRenderable.setString(String.format(TEXT_ENERGY, getEnergy()));
     }
 }
