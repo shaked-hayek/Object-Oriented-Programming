472b719932593a5342bedb3d6936afb8c3426094
unknown <shaked.hayek@mail.huji.ac.il>
2023-01-07 11:27:57 +0200

    Avatar flying (still not good)

--- src/pepse/world/Avatar.java
+++ src/pepse/world/Avatar.java
@@ -2,8 +2,6 @@ package pepse.world;
 
 import danogl.GameObject;
 import danogl.collisions.GameObjectCollection;
-import danogl.components.ScheduledTask;
-import danogl.components.Transform;
 import danogl.gui.ImageReader;
 import danogl.gui.UserInputListener;
 import danogl.gui.rendering.Renderable;
@@ -16,18 +14,23 @@ public class Avatar extends GameObject {
     public static final float AVATAR_HEIGHT = 34;
     public static final float AVATAR_WIDTH = 30;
     public static final float INIT_ENERGY = 100;
-    public static final double ENERGY_SUBTRACT = 0.5;
+    public static final float MIN_ENERGY = 0;
+    public static final double ENERGY_CHANGE = 0.5;
     public static final int VELOCITY_X = 300;
-    public static final int VELOCITY_Y = -300;
-    public static final int ACCELERATION_Y = 500;
+    public static final int VELOCITY_Y = 300;
+    public static final int ACCELERATION_Y = 400;
+    public static final int FLY_Y = 100;
     private static final String AVATAR_IMG_FOLDER = "assets/";
     private static final String AVATAR_STANDING = "avatar_standing.png";
     private static final String TAG_NAME = "avatar";
     private static UserInputListener inputListener;
+    private static double energy;
+    private static boolean flyingMode;
 
     public Avatar(Vector2 topLeftCorner, Vector2 dimensions, Renderable renderable) {
         super(topLeftCorner, dimensions, renderable);
-
+        energy = INIT_ENERGY;
+        flyingMode = false;
         transform().setAccelerationY(ACCELERATION_Y);
     }
 
@@ -48,23 +51,57 @@ public class Avatar extends GameObject {
         return avatar;
     }
 
+    private void horizontalMovement() {
+        transform().setVelocityX(0);
+        // Move left
+        if (inputListener.isKeyPressed(KeyEvent.VK_LEFT)) {
+            renderer().setIsFlippedHorizontally(true);
+            transform().setVelocityX(-VELOCITY_X);
+        }
+        // Move right
+        if (inputListener.isKeyPressed(KeyEvent.VK_RIGHT)) {
+            renderer().setIsFlippedHorizontally(false);
+            transform().setVelocityX(VELOCITY_X);
+        }
+    }
+
+    private void jump() {
+        if (inputListener.isKeyPressed(KeyEvent.VK_SPACE) && getVelocity().y() == 0) {
+            transform().setVelocityX(0);
+            transform().setVelocityY(-VELOCITY_Y);
+        }
+    }
+
+    private void fly() {
+        if (inputListener.isKeyPressed(KeyEvent.VK_SPACE) &&
+                inputListener.isKeyPressed(KeyEvent.VK_SHIFT) &&
+                energy > MIN_ENERGY) {
+            transform().setVelocityY(- (VELOCITY_Y + FLY_Y));
+            flyingMode = true;
+        } else {
+            flyingMode = false;
+        }
+    }
+
+    private void updateEnergy() {
+        if (flyingMode) {
+            if (energy < INIT_ENERGY) {
+                energy += ENERGY_CHANGE;
+            }
+        } else {
+            if (energy > MIN_ENERGY) {
+                energy -= ENERGY_CHANGE;
+            }
+        }
+    }
+
     @Override
     public void update(float deltaTime) {
         super.update(deltaTime);
-        float xVel = 0;
-        if(inputListener.isKeyPressed(KeyEvent.VK_LEFT))
-            xVel -= VELOCITY_X;
-        if(inputListener.isKeyPressed(KeyEvent.VK_RIGHT))
-            xVel += VELOCITY_X;
-        transform().setVelocityX(xVel);
-        if(inputListener.isKeyPressed(KeyEvent.VK_SPACE) && inputListener.isKeyPressed(KeyEvent.VK_DOWN)) {
-            physics().preventIntersectionsFromDirection(null);
-            new ScheduledTask(this, .5f, false,
-                    ()->physics().preventIntersectionsFromDirection(Vector2.ZERO));
-            return;
-        }
-        if(inputListener.isKeyPressed(KeyEvent.VK_SPACE) && getVelocity().y() == 0)
-            transform().setVelocityY(VELOCITY_Y);
+        horizontalMovement();
+        jump();
+        fly();
+        updateEnergy();
     }
 
 }
