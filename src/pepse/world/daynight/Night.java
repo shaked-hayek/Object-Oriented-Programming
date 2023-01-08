2d91ed109853bf9d3b8166dd962347767afe0dff
shirlevy <shir.levy2@mail.huji.ac.il>
2023-01-08 14:48:02 +0200

    Night - documented

--- src/pepse/world/daynight/Night.java
+++ src/pepse/world/daynight/Night.java
@@ -12,9 +12,23 @@ import pepse.util.ColorSupplier;
 import java.awt.Color;
 
 public class Night {
+    /**
+     * darkest opacity
+     */
     private static final float MIDNIGHT_OPACITY = 0.5f;
-    private static final String TAG_NAME = "night";
+    /**
+     * night name tag
+     */
+    public static final String TAG_NAME = "night";
 
+    /**
+     * creates an object of night in the game
+     * @param gameObjects the collection of all game objects currently in the game
+     * @param layer of night rectangle
+     * @param windowDimensions of game
+     * @param cycleLength for the night to return
+     * @return night game object
+     */
     public static GameObject create(GameObjectCollection gameObjects,
                                     int layer,
                                     Vector2 windowDimensions,
