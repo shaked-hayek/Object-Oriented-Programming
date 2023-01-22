a19a6fb9bd150da137c65888f396b43278b9718b
unknown <shaked.hayek@mail.huji.ac.il>
2023-01-22 12:50:54 +0200

    Add doc to variables

--- src/oop/ex6/componants/variables/Variables.java
+++ src/oop/ex6/componants/variables/Variables.java
@@ -6,6 +6,9 @@ import oop.ex6.componants.methods.Scope;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * Class for handling a line of few variables.
+ */
 public class Variables {
     private static final String NAME_REGEX = "([a-zA-Z]+[a-zA-Z0-9_]*|_+[a-zA-Z0-9_]+)";
     private static final String FINAL_REGEX = "\\s*final\\s+";
@@ -17,10 +20,23 @@ public class Variables {
     private Scope scope;
 
 
+    /**
+     * Constructor.
+     * @param scope the scope the variables are in.
+     */
     public Variables(Scope scope) {
         this.scope = scope;
     }
 
+    /**
+     * Process a line of initialization or assigment to variables and add them to scope.
+     * @param line the line from file without ;.
+     * @throws InvalidVarTypeException              The var type doesn't exist.
+     * @throws InvalidVarDeclarationException       The var declaration doesn't match the rules.
+     * @throws ValueMismatchException               The value of var doesn't match the var type.
+     * @throws VarNameInitializedException          Variable initialization with illegal name.
+     * @throws IllegalFinalVarAssigmentException    Illegal assigment to a final variable.
+     */
     public void processVarsLine(String line) throws InvalidVarTypeException, InvalidVarDeclarationException,
             ValueMismatchException, VarNameInitializedException, IllegalFinalVarAssigmentException {
         // Check final
@@ -36,15 +52,30 @@ public class Variables {
         }
     }
 
+    /**
+     * Checks if final word in line.
+     * @param line  line as string.
+     * @return True if final in line, false otherwise.
+     */
     public static boolean isFinal(String line) {
         Matcher m = FINAL_PATTERN.matcher(line);
         return m.lookingAt();
     }
 
+    /**
+     * Strip the word final from the line.
+     * @param line line as string.
+     * @return The line without the final word.
+     */
     public static String stripFinal(String line) {
         return line.replaceAll(FINAL_REGEX, "");
     }
 
+    /**
+     * Checks if line is as initialization line by checking if the first word is a var type.
+     * @param line  The line as string.
+     * @return  True if initialization line, false otherwise.
+     */
     private boolean isInitialization(String line) {
         Pattern p = Pattern.compile("\\s*" + VarTypeFactory.getTypeRegex());
         Matcher m = p.matcher(line);
@@ -54,10 +85,10 @@ public class Variables {
     /**
      * Get an initialization line of shape "<type> <var>(=<value>)" check it validity and save vars to scope.
      * @param initLine initialization line
-     * @throws InvalidVarTypeException
-     * @throws InvalidVarDeclarationException
-     * @throws VarNameInitializedException
-     * @throws ValueMismatchException
+     * @throws InvalidVarTypeException          The var type doesn't exist.
+     * @throws InvalidVarDeclarationException   The var declaration doesn't match the rules.
+     * @throws VarNameInitializedException      Variable initialization with illegal name.
+     * @throws ValueMismatchException           The value of var doesn't match the var type.
      */
     private void varInitialization(String initLine)
             throws InvalidVarTypeException, InvalidVarDeclarationException, VarNameInitializedException,
@@ -80,6 +111,13 @@ public class Variables {
         }
     }
 
+    /**
+     * Get an assigment line of shape "<var>(=<value>)" check it validity and update initialization flag.
+     * @param assignLine    assigment line.
+     * @throws InvalidVarDeclarationException       The var declaration doesn't match the rules.
+     * @throws IllegalFinalVarAssigmentException    Illegal assigment to a final variable.
+     * @throws ValueMismatchException               The value of var doesn't match the var type.
+     */
     private void varAssigment(String assignLine)
             throws InvalidVarDeclarationException, IllegalFinalVarAssigmentException, ValueMismatchException {
         String[] vars = splitVarsLine(assignLine);
@@ -101,6 +139,12 @@ public class Variables {
         }
     }
 
+    /**
+     * Split vars line by ",".
+     * @param line  line of vars
+     * @return  a list of var strings
+     * @throws InvalidVarDeclarationException   Thrown when no vars in string
+     */
     private String[] splitVarsLine(String line)
             throws InvalidVarDeclarationException {
         String[] vars = line.split(",");
