0682fe2dcc60f1813ce5d6cd1a282d0b58105808
unknown <shaked.hayek@mail.huji.ac.il>
2023-01-22 14:59:49 +0200

    Method validator

--- src/oop/ex6/componants/MethodValidator.java
+++ src/oop/ex6/componants/MethodValidator.java
@@ -1,2 +1,11 @@
-package oop.ex6.componants;public class MethodValidator {
+package oop.ex6.componants;
+
+import oop.ex6.componants.methods.Method;
+import oop.ex6.componants.methods.Scope;
+
+public class MethodValidator {
+
+    public MethodValidator(Scope globalScope) {
+
+    }
 }
